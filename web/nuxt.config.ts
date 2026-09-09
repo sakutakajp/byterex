6e59ea4aa28dcac0679f864b6e98193656454659
sakutakajp <git@noreply.sakutaka.jp>
2026-09-10 01:02:05 +0900

    Update nuxt.config.ts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -5,6 +5,16 @@ export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/test-utils"],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+    },
+  },
+  // runtimeConfig: {
+  //   public: {
+  //     gtmId: "",
+  //   },
+  // },
   nitro: {
     prerender: {
       routes: ["/ja", "/ja/about", "/en", "/en/about"],
