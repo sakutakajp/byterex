4d9256955a02662537bec961ebef7c01141e1903
sakutakajp <git@noreply.sakutaka.jp>
2026-09-10 16:38:03 +0900

    Create ProseTable.vue

---
+++ web/app/components/content/ProseTable.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="my-7 overflow-x-auto rounded-[10px] border border-neutral-200 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-neutral-700"
+    tabindex="0"
+  >
+    <table class="!m-0 w-full">
+      <slot />
+    </table>
+  </div>
+</template>
